2d6849d66a2fc25baee8c63eb058dd38664f3906
12izal <syaifoelrizal9090@gmail.com>
2026-07-01 11:08:53 +0700

    Add favicon support

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -15,6 +15,12 @@
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
